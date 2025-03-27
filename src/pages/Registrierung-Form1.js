da6dfc76a56a05491e4f52948cf6911c0034ed1d
theekspertsteam <info@the-eksperts.com>
2025-03-27 08:51:42 +0100

    Update Registrierung-Form1.js

--- src/pages/Registrierung-Form1.js
+++ src/pages/Registrierung-Form1.js
@@ -46,6 +46,7 @@ export default function FormPage01() {
        return (
          formData.region &&
          formData.name &&
+         formData.vorname &&
          emailRegex.test(formData.email) &&
          formData.cv
        );
@@ -64,7 +65,7 @@ export default function FormPage01() {
      };
   return (
 
-    <div className="bg-[#F1F1F1] text-[#B99B5F] min-h-screen p-4">
+    <div className="bg-[#FAFCFF] text-[#B99B5F] min-h-screen p-4">
       {/* Section 1: Logo and Zuruk */}
       <section className="lg:block hidden lg:flex justify-center items-center pt-[25px] md:pt-[25px]">
         <div className="absolute top-6 left-4 lg:left-[170px]">
@@ -245,6 +246,17 @@ Wählen Sie die gewünschte <br></br>Region und geben Sie<br></br>Ihre E-Mail Ad
           }}
           value={formData.name}
           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        />
+          <input
+          type="text"
+          placeholder="Vorname"
+          required
+          className="w-[271px] h-[75px] px-[13px] py-[17px] text-[18px] border border-[#B7B6BA] rounded-lg bg-white text-[#1C1B1D]  placeholder-[#1C1B1D]"
+          style={{
+            fontFamily: "Metropolis",
+          }}
+          value={formData.vorname}
+          onChange={(e) => setFormData({ ...formData, vorname: e.target.value })}
         />
         <input
           type="email"
